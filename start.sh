06071bd17695d488508a4eb0ff25b25ff9d16478
tlack <lackner@gmail.com>
2015-05-08 17:45:05 -0400

    start.sh: on dev, remove database at start

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+ENV=dev
+
 DEV="-l -e 1 -p 8080"
 PROD="-l -p 80"
-ARGS=$DEV
+
+if [ x$ENV = xdev ]; then
+	rm qwa.log qwa.qdb
+	ARGS=$DEV
+else
+	ARGS=$PROD
+fi
+
 rlwrap ~/q/l32/q qwa $* $ARGS
 
 
